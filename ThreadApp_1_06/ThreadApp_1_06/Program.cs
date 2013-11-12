3db9a21f4110610f414ef14c74fff5d1daf8e88b
Thiago <supercoja@gmail.com>
2013-11-12 13:04:18 -0200

    Commit - Exercicio 1 - 06

---
+++ ThreadApp_1_06/ThreadApp_1_06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadApp_1_06
+{
+    public static class Program
+    {
+
+        public static void ThreadMethod()
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
