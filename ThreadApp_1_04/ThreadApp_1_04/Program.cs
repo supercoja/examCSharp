6d757e2a38e4da7ed47b234feb95b93ad2dd9630
Thiago <supercoja@gmail.com>
2013-11-12 12:37:32 -0200

    First Commit - 30/03/2013

---
+++ ThreadApp_1_04/ThreadApp_1_04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadApp_1_04
+{
+    public static class Program
+    {
+        public static void ThreadMethod(object _o)
+        {
+            for (int i = 0; i < (int)_o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0} ", i);
+                Thread.Sleep(0); // sinal de aviso ao windows que a Thread foi finalizada e que pode ser liberado o "time slice" alocado para a thread 
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            bool _stopped = false;
+
+            Thread _t = new Thread(new ThreadStart(() =>
+            {
+                while (!_stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+            }));
+            _t.Start();
+            Console.WriteLine("Press Any Key To Exit");
+            Console.ReadKey();
+
+            _stopped = true;
+
+            _t.Join(); // faz com que o processo rode ate que seja finalizada a outra thread
+        }
+    }
+}
---
+++ ThreadStaticField_1_05/ThreadStaticField_1_05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadStaticField_1_05
+{
+    public static class Program
+    {
+//        [ThreadStatic]
+        public static int _field;
+
+        public static void Main(string[] args)
+        {
+            new Thread(() =>
+                {
+                    for (int x = 0;x<10;x++)
+                    {
+                        _field++;
+                        Console.WriteLine("Thread A: {0}", _field);
+                    }
+                }).Start();
+            new Thread(() =>
+                {
+                    for (int x = 0; x < 10; x++)
+                    {
+                        _field++;
+                        Console.WriteLine("Thread B: {0}", _field);
+                    }
+                }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
