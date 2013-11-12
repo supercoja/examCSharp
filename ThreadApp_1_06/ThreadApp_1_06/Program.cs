fd3621f7be28dab405397d91ab0b4241ce18ba0e
Thiago <taalmeid@yahoo.com.br>
2013-11-12 16:48:56 -0200

    Commit Thread App 6 completa

---
+++ ThreadApp_1_06/ThreadApp_1_06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ThreadApp_1_06
+{
+    public static class Program
+    {
+        public static ThreadLocal<int> _field =  new ThreadLocal<int>(() =>
+        {
+//            return new Thread.CurrentThread.ManagedThreadId;
+        });
+    
+        public static void Main(string[] args)
+        {
+            new Thread(() =>
+                {
+                    for (int x=0;x>_field.Value;x++)
+                    {
+                        Console.WriteLine("Thread A: {0}  ",x);
+                    }
+                }).Start();
+            new Thread(() =>
+                {
+                    for (int x=0; x < _field.Value; x++)
+                    {
+                        Console.WriteLine("Thread B: {0}", x);
+                    }
+                }).Start();
+
+            Console.ReadKey;
+
+        }
+    }
+}
