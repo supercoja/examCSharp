b6630bccdfbbe3a43d239e2efead0298d2eb9dfd
supercoja <taalmeid@yahoo.com.br>
2013-12-24 17:25:03 -0200

    Commit II - Thiago - 24/12/2014 - 17h24min

--- ThreadApp_1_10/ThreadApp_1_10/Program.cs
+++ ThreadApp_1_10/ThreadApp_1_10/Program.cs
@@ -4,19 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ThreadApp_1_09
+namespace ThreadApp_1_10
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
             Task<int> t = Task.Run(() =>
-                {
-                    return 42;
-                });
+            {
+                return 42;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            });
             Console.WriteLine(t.Result);
             Console.ReadLine();
         }
-
     }
 }
