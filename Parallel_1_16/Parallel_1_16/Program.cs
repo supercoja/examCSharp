921a62e7f9d15fdebd133dbbf42c57d1b4ba9934
Thiago <supercoja@gmail.com>
2013-12-25 23:35:12 -0200

    Commit Exercise 16 - Thiago - Teste

---
+++ Parallel_1_16/Parallel_1_16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel_1_16
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ParallelLoopResult _result = Parallel.For(0, 1000, (int i, ParallelLoopState _loopState) =>
+                {
+                    Console.WriteLine(i);
+                    if (i == 500)
+                    {
+                        Console.WriteLine("Breaking Loop");
+                        _loopState.Break();
+                    }
+//                    Console.ReadKey();
+                });
+        }
+    }
+}
