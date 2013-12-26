48d4612ab62a48b23b13c8cb3fa37461a78365a9
Thiago <supercoja@gmail.com>
2013-12-25 23:13:03 -0200

    Commit Exercicio 15 - Thiago - 23h12min

---
+++ ThreadApp_1_15/ThreadApp_1_15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadApp_1_15
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task<int>[] _tasks = new Task<int>[3];
+
+            _tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            _tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            _tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+
+            while (_tasks.Length > 0)
+            {
+                int i = Task.WaitAny(_tasks);
+                Task<int> _completedTask = _tasks[i];
+                Console.WriteLine(_completedTask.Result);
+
+                var _temp = _tasks.ToList();
+                _temp.RemoveAt(i);
+                _tasks = _temp.ToArray();
+            }
+//            Console.ReadKey();
+        }
+    }
+}
