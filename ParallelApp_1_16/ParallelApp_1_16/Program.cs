cd7eb2c3e632850e2319ca1be48dbe11e2ac3b4d
Thiago <supercoja@gmail.com>
2013-12-25 23:24:15 -0200

    Commit Exercise 16 - Parallels - Thiago 23h24min

---
+++ ParallelApp_1_16/ParallelApp_1_16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelApp_1_16
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parallel .For(0,10,i => 
+                {
+                    Thread.Sleep(1000);
+                });
+            var _numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(_numbers,i =>
+            {
+                Thread.Sleep(1000);
+            });
+
+        }
+    }
+}
