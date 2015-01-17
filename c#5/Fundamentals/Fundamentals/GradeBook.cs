0126fa6fac328892cde912fb92d786c829e91324
Thiago <taalmeid@yahoo.com.br>
2015-01-17 15:04:26 -0200

    Fundamentals Delegates

---
+++ c#5/Fundamentals/Fundamentals/GradeBook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            _grades = new List<float>();
+        }
+
+        public void AddGrade(float grade)
+        {
+            _grades.Add(grade);
+        }
+        public GradeStatistics ComputeStatistics()
+        {
+            return new GradeStatistics();
+        }
+
+        List<float> _grades;
+
+
+
+    }
+}
---
+++ c#5/Fundamentals/Fundamentals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gradeBook = new GradeBook();
+            gradeBook.AddGrade(91f);
+            gradeBook.AddGrade(89.1f);
+
+            GradeStatistics statistics = gradeBook.ComputeStatistics();
+
+
+            var gradeBook2 = new GradeBook();
+            gradeBook2.AddGrade(75f);
+            gradeBook2.AddGrade(55.5f);
+
+
+            
+        }
+    }
+}
